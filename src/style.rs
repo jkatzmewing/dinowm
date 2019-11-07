7bfb23574c486e3fcede83013b4870a96d956b5d
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2019-11-06 19:34:23 -0500

    Add color specs to Style struct, and update example config

--- src/style.rs
+++ src/style.rs
@@ -2,5 +2,14 @@
 pub struct Style {
     pub border_width: u8,
     pub titlebar_width: u8,
+
+    pub titlebar_color_bg: u32,
+    pub titlebar_color_fg: u32,
+
+    pub border_color_bg: u32,
+    pub border_color_fg: u32,
+
+    pub text_color_bg: u32,
+    pub text_color_fg: u32,
 }
 
