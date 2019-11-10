75b9eaf8ccb6e25c7ca1e325d052f6aa96549715
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2019-11-10 13:44:13 -0500

    First implementation of frame and titlebar drawing

--- src/windows.rs
+++ src/windows.rs
@@ -1,14 +1,47 @@
 use xcb;
 
+use std::convert::TryInto;
+
 use crate::style::Style;
 use crate::xorg::Xorg;
 
 fn draw_window_frame(xorg: &Xorg, window: xcb::Window, style: &Style) -> xcb::Window {
-    std::unimplemented!()
+    let frame = xorg.connection.generate_id();
+    xcb::create_window(
+        xorg.connection,
+        xcb::COPY_FROM_PARENT as u8,
+        frame,
+        xorg.screen.root(),
+        0,
+        0, // TODO smarter window placement
+        100,
+        100, // TODO actually get width/height from reparented window
+        0,
+        0, // TODO figure out the correct stuff to put here
+        xcb::COPY_FROM_PARENT,
+        &[],
+    );
+    frame
 }
 
 fn draw_window_titlebar(xorg: &Xorg, window: xcb::Window, style: &Style) -> xcb::Window {
-    std::unimplemented!()
+    let borders: u16 = (style.border_width * 2).try_into().unwrap();
+    let titlebar = xorg.connection.generate_id();
+    xcb::create_window(
+        xorg.connection,
+        xcb::COPY_FROM_PARENT as u8,
+        titlebar,
+        xorg.screen.root(),
+        style.border_width, // TODO smarter window placement
+        style.border_width,
+        100 - borders, // TODO get width from reparented window
+        100 - borders,
+        0,
+        0, // TODO figure out the correct stuff to put here
+        xcb::COPY_FROM_PARENT,
+        &[],
+    );
+    titlebar
 }
 
 pub fn reparent_window(xorg: &Xorg, ev: &xcb::CreateNotifyEvent, style: &Style) {
