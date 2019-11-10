800346f4d540b2859556c362ab588c858f676743
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2019-11-10 10:05:59 -0500

    Start implementing reparenting and window drawing

--- src/style.rs
+++ src/style.rs
@@ -2,8 +2,8 @@ use xcb::render::Color;
 
 // TODO implement fonts and colors
 pub struct Style {
-    pub border_width: u8,
-    pub titlebar_height: u8,
+    pub border_width: i16,
+    pub titlebar_height: i16,
 
     pub titlebar_color_bg: Color,
     pub titlebar_color_fg: Color,
--- src/windows.rs
+++ src/windows.rs
@@ -3,6 +3,29 @@ use xcb;
 use crate::style::Style;
 use crate::xorg::Xorg;
 
+fn draw_window_frame(xorg: &Xorg, window: xcb::Window, style: &Style) -> xcb::Window {
+    std::unimplemented!()
+}
+
+fn draw_window_titlebar(xorg: &Xorg, window: xcb::Window, style: &Style) -> xcb::Window {
+    std::unimplemented!()
+}
+
 pub fn reparent_window(xorg: &Xorg, ev: &xcb::CreateNotifyEvent, style: &Style) {
-    std::unimplemented!();
+    let titlebar = draw_window_titlebar(xorg, ev.window(), style);
+    let frame = draw_window_frame(xorg, ev.window(), style);
+    xcb::reparent_window(
+        xorg.connection,
+        titlebar,
+        frame,
+        style.border_width,
+        style.border_width,
+    );
+    xcb::reparent_window(
+        xorg.connection,
+        ev.window(),
+        frame,
+        style.border_width,
+        (style.border_width * 2) + style.titlebar_height,
+    );
 }
